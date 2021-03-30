46231f85ebef42e71065c3223ce12c48bacb4e01
Raphael Matori <raphaelmatori@hotmail.com>
2021-03-30 14:30:01 -0300

    Loop ao sair da tela

--- script.js
+++ script.js
@@ -20,7 +20,23 @@ const createSnake = () => {
     }
 }
 
+
+const update = (event) => {
+    if (event.keyCode == 37 && direction != "right") direction = "left";
+    if (event.keyCode == 38 && direction != "down") direction = "up";
+    if (event.keyCode == 39 && direction != "left") direction = "right";
+    if (event.keyCode == 40 && direction != "up") direction = "down";
+}
+
+document.addEventListener('keydown', update);
+
 const initGame = () => {
+
+    if (snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+    if (snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
+    if (snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
+    if (snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+
     createBackground();
     createSnake();
     let snakeX = snake[0].x;
